1c49c498a3b880b3a1c0de8bd73ddd6d00b57c0c
Prakhar <prakhar@coloredcow.in>
2016-04-07 02:49:46 +0530

    check with existing keyword data

--- app/Http/Controllers/KeywordController.php
+++ app/Http/Controllers/KeywordController.php
@@ -9,6 +9,8 @@ use App\SearchedKeyword;
 use App\SearchedUrl;
 use App\Keydata;
 use Carbon\Carbon;
+use DB;
+use DateTime;
 use \SEOstats\Services as SEOstats;
 use App\GoogleKeywordsRank;
 
@@ -184,42 +186,110 @@ class KeywordController extends Controller
     */
     public function foo() {
 
-        $query = 'furnace%20calgary';
-        $domain = "emergencyfurnacerepaircalgary.com";
+        if( ctype_digit($id) ){
+            $data = SearchedKeyword::findorFail($id);
+            $keyword = $data->keyword;
+            $domain = $data->url;
 
-        //To get top 100 results. Only 8 results at a time.
-        for($i = 0; $i < 50; $i += 8) {
-        $url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=".$query.'&rsz=large'.'&start='.$i.'&num=100'.'&userip='.$_SERVER['REMOTE_ADDR'];
+            $urlid = SearchedUrl::fetchId($domain)['id'];
 
-        $body = file_get_contents($url);
-        $json = json_decode($body);
+            $results = array();
+            $res = array();
+            $fetch = array();
 
-        $urls = [];
-        $found = 0;
-        $rank = 0;
-        $counter = 0;
+            for ( $start = ( $this->start-1 ) * 10; $start <= $this->end * 10; $start += 10 ) {
+                $ua	= array(
+                    0 	=> 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+                    10 	=> 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+                    20 	=> 'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0;  rv:11.0) like Gecko'
+                );
 
-        for($x=0;$x<count($json->responseData->results);$x++){
-            $urls[$counter] = $json->responseData->results[$x]->visibleUrl;
+                $options = array(
+                    "http" => array(
+                        "method" => "GET",
+                        "header" => "Accept-language: en\r\n" .
+                            "Cookie: SEO Zen\r\n" .
+                        "User-Agent: " . $ua[ $start ] )
+                );
 
-            //Check if URL matches
-            if($found == 0 && strpos($urls[$counter], $domain) !== false) {
-                $found = 1;
-                $rank = $counter+1;
+                $keyword = str_replace( ' ', '+', trim( $keyword ) );
+                $url = 'https://www.google.com/search?ie=UTF-8&q=' . $keyword . '&start=' . $start . '&num=30';
+                $context = stream_context_create( $options );
+
+                if ( $this->_isCurl() ) {
+                    $data = $this->_curl( $url );
+                } else {
+                    $data	= @file_get_contents( $url, false, $context );
+                }
+                var_dump($data);
+                if ( is_array( $data ) ) {
+                    $errmsg = $data['errmsg'];
+                    $results = array( 'rank' => 'zerox', 'url' => $errmsg );
+                    $check = 'fail';
+                } else {
+                    if ( strpos( $data, 'To continue, please type the characters below' ) !== FALSE || $data == FALSE || strpos( $data, "We're sorry" ) !== FALSE ) {
+                        $results = array( 'rank' => 'zero', 'url' => '' );
+                        $check = 'fail';
+                    } else {
+                        $j = -1;
+                        $i = 1;
+                        $check = 'success';
+
+                        while( ( $j = stripos( $data, '<cite class="_Rm">', $j+1 ) ) !== false ) {
+                            $k = stripos( $data, '</cite>', $j );
+                            $link = strip_tags( substr( $data, $j, $k-$j ) );
+                            $rank	= $i++;
+                            $results[] = array( 'rank' => $rank, 'url' => $link );
+                        }
+
+                        var_dump($i); die();
+                    }
+                }
             }
-            echo " ".$found;
-        }
 
-        }
+            if($check == 'success'){
+                $i = 1;
+                $rank = 10000000;
+                var_dump($domain."\n");
+                foreach( $results as $i ){
+                    var_dump("\n".$i['url']);
+                    if (strpos($i['url'], $domain)){
+                        $rank = $i['rank'];
+                        var_dump("==hey".$rank."==");
+                    }
+                    if( $i <= 10 )
+                        $res[] = array( 'rank' => $i['rank'], 'url' => $i['url'] );
+                    $i++;
+                }
 
-        print_r($rank);
-        echo "/n";
-        print_r($urls);
-        die();
+                /*if(count($res) > 0){
+                    $sort_col = array();
+                    foreach ($res as $key=> $row) {
+                        $sort_col[$key] = $row['rank'];
+                    }
+
+                    array_multisort($sort_col, SORT_ASC, $res);
+                    */
+
+                    $store = new Keydata;
+                    $store->key_id = $id;
+                    $store->keyword_rank = $rank;
+                    $store->searched_at = Carbon::now();
+                    $store->save();
+            }
+            $fetch = Keydata::where('key_id', $id)->get();
+            var_dump($res);
+            die();
+            return view('pages.keyword_data', compact('keyword', 'rank', 'res', 'check', 'domain', 'urlid', 'fetch'));
+        }
+        else
+            return view('pages.error');
     }
 
     public function find($id) {
+
         if( ctype_digit($id) ){
+
             $data = SearchedKeyword::findorFail($id);
             $keyword = $data->keyword;
             $domain = $data->url;
@@ -237,18 +307,61 @@ class KeywordController extends Controller
             $res = [];
             $urls = [];
 
+            $saveurls = "";
+            $nw = new DateTime(Carbon::now());
+            $nw->sub(new \DateInterval('P7D'));
+
+            //Checking keyword history and time elapsed.
+            $temp = DB::table('storekeyurls')->where('keywordname', $keyword)
+                ->where('latestcheck', '>', $nw)
+                ->first();
+
+
+
+            if(sizeof($temp) > 0) {
+                $ans = $temp->urls;
+                $token = strtok($ans, "*");
+                $cc = 0;
+                    while ($token !== false)
+                    {
+                    $res[$cc] = $token;
+                    $token = strtok("*");
+                    $cc++;
+                    }
+
+
+                $rank = $temp->latestrank;
+
+                //store in keydata
+                $store = new Keydata;
+                $store->key_id = $id;
+                $store->keyword_rank = $rank;
+                $store->searched_at = Carbon::now();
+                $store->save();
+            }
+            else {
             $KEY = "AIzaSyCgEhwLRxr2-dN68_x58XMSsLelpKJxTxA";
             $CSE = "003799387166088970884:mguauzeslus";
 
-           var_dump($_SERVER['REMOTE_ADDR']);
+           //var_dump($_SERVER['REMOTE_ADDR']);
 
-                for($i = 0; $i < 30; $i += 8) {
+                for($i = 0; $i <= 40; $i += 8) {
 
-                    $last = rand(10, 254);
+                    $first = rand(40, 240);
+                    $second = rand(20, 250);
                     $slast = rand(10, 254);
-                    $ip = "51.203.".$last.".".$slast;
+                    $last = rand(10, 254);
+                    $ip = $first.".".$second.".".$slast.".".$last;
 
-                    $url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=".$query."&rsz=large"."&start=".$i."&userIp=".$ip;
+                    //generate random string
+                    $newstr = "";
+                    $len = rand(10, 15);
+                    for($d = 0; $d < $len; $d++) {
+                        $rvar = rand(97, 123);
+                        $newstr .= chr($rvar);
+                    }
+
+                    $url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=".$query."&rsz=large"."&start=".$i."&userIp=".$ip."&userip=".$ip."&rand=".$newstr;
 
                     $body = file_get_contents($url);
                   //  var_dump($body);
@@ -261,6 +374,7 @@ class KeywordController extends Controller
                     }
                     for($x=0;$x<count($json->responseData->results);$x++){
                         $res[$counter] = $json->responseData->results[$x]->visibleUrl;
+                        $saveurls .= $res[$counter]."*";
 
                         //Check if URL matches
                         if($check == 0 && strpos($res[$counter], $domain) !== false) {
@@ -271,18 +385,37 @@ class KeywordController extends Controller
                          $counter++;
                     }
 
-                    $ssleep = 10;
-                   // sleep($ssleep);
+                    $ssleep = 5;
+                    sleep($ssleep);
                 }
-            var_dump($rank);
 
             if($error !== "multiple") {
-            $store = new Keydata;
-            $store->key_id = $id;
-            $store->keyword_rank = $rank;
-            $store->searched_at = Carbon::now();
-            $store->save();
+                $store = new Keydata;
+                $store->key_id = $id;
+                $store->keyword_rank = $rank;
+                $store->searched_at = Carbon::now();
+                $store->save();
+
+                $x = Carbon::now();
+
+                //if old keyword
+                if(sizeof($temp) > 0) {
+                    //var_dump("saving old");
+                DB::table('storekeyurls')
+                    ->where('keywordname', $keyword)
+                    ->update(['urls' => $saveurls, 'latestcheck' => $x, 'latestrank' => $rank]);
+                }
+                else {
+                //Save keyword data as new
+                DB::table('storekeyurls')->insert([
+                    ['keywordname' => $keyword, 'urls' => $saveurls, 'latestcheck' => $x, 'latestrank' => $rank]
+                ]);
+                }
+
             }
+
+            }
+
             $fetch = Keydata::where('key_id', $id)->get();
 
             return view('pages.keyword_data', compact('keyword', 'rank', 'res', 'found', 'domain', 'urlid', 'fetch', 'error', 'ip'));
