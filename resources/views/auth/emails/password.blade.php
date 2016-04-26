063862e8b4cf047b3c017a1ffe7654215757e9ad
Prakhar <prakhar@coloredcow.in>
2016-04-26 23:44:33 +0530

    Password reset mail

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,2 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+<h1>SEOPlus</h1>
+<p>Click here to reset your password: <br><br><a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a></p>
